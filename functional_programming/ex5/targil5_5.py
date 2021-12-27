93780dffc2bda6962b574d6191f2acff6a351c4f
maorsarusi <msarusi18@gmail.com>
2021-12-27 23:07:29 +0200

    finish ex 5 is another version to previous ex

--- functional_programming/ex5/targil5_5.py
+++ functional_programming/ex5/targil5_5.py
@@ -1,35 +1,37 @@
 import random
-peopleNames=("Bar","Rotem","Jenifer","Yarden","Bridgit","Nataly")
-verbs=("sees","eats","sleep","sings")
-adjectives=("tall","small","wide","red","delicious")
-animateObjects=("flower","oranges","apples")
-adverbs=("slowly","tommorow","soon","today")
-inanimateObjects=(" chicken meat"," stone"," chair")
+
+peopleNames = ("Bar", "Rotem", "Jenifer", "Yarden", "Bridgit", "Nataly")
+verbs = ("sees", "eats", "sleep", "sings")
+adjectives = ("tall", "small", "wide", "red", "delicious")
+animateObjects = ("flower", "oranges", "apples")
+adverbs = ("slowly", "tommorow", "soon", "today")
+inanimateObjects = (" chicken meat", " stone", " chair")
 # peopleName->adverbs->verbs->adjectives->[animateObjects || inanimateObjects]
-wordsList = [peopleNames,adverbs,verbs,adjectives,[animateObjects,inanimateObjects]]
+wordsList = [peopleNames, adverbs, verbs, adjectives, [animateObjects, inanimateObjects]]
+
+
 #
 def prtPoem(poem):
-     return list(print(' '.join(line))  for line in poem)
-    
+    return list(print(' '.join(line)) for line in poem)
+
 
 def chooseWord(words):
-    if isinstance(words,list):
-        w = words[random.randrange(0,2)]
+    if isinstance(words, list):
+        w = words[random.randrange(0, 2)]
     else:
-        w = words       
-    return w[random.randrange(0,len(words))]    
+        w = words
+    return w[random.randrange(0, len(words))]
 
-def crPoem(N,shir =[]):
-    if N ==0:
+
+def crPoem(N, shir=[]):
+    if N == 0:
         return shir
     else:
-        return crPoem(N-1,shir + [list(map(chooseWord,wordsList))])
-    
-
+        return crPoem(N - 1, shir + [list(map(chooseWord, wordsList))])
 
 
 def theHumblePoet(N):
-    if isinstance(N,int) and N>0:
+    if isinstance(N, int) and N > 0:
         prtPoem(crPoem(N))
     else:
         print("ERROR,must be a positive number")
